2929ee9821e263c084118232ddcd312c78924feb
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2022-04-25 15:14:49 -0500

    Added a spawner for NPCs
    This is to make town look lively

---
+++ Assets/place holder/InClassTutorial1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    [SerializeField]
+    private GameObject EndOFRoad;
+    private float EndOfRoadZValue;
+    private float turnspeed = 200.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    void Start()
+    {
+        Transform EndOfRoadTransform = EndOFRoad.GetComponent<Transform>();
+        EndOfRoadZValue = EndOfRoadTransform.position.z;
+    }
+
+    void Update()
+    {
+        if (this.transform.position.z >= EndOfRoadZValue)
+            return;
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up, turnspeed * horizontalInput * Time.deltaTime);
+    }
+}
