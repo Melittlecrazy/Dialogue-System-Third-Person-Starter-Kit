2b57989d6c514a64960b4d8c2e61328795d25348
Marc Gutierrez <55261795+Melittlecrazy@users.noreply.github.com>
2022-05-01 21:40:10 -0500

    Adding the towns people
    It's a mess but it's there.

--- Assets/Scripts/AgentController.cs
+++ Assets/Scripts/AgentController.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class AgentController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                this.GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            }
+        }
     }
 }
